31c59fc2a0ac689207c5e7f781ebcc38171c4ed2
J_Coder <seok3765@naver.com>
2023-12-05 18:44:34 +0900

    Revert "우왕"
    This reverts commit b1cd1f5c7d1cb1e173ffebdf8c7f46f354d4a738.

--- 비주얼프로젝트_20222940박경민/비주얼프로젝트_20222940박경민/UI/LOGIN/register.Designer.cs
+++ 비주얼프로젝트_20222940박경민/비주얼프로젝트_20222940박경민/UI/LOGIN/register.Designer.cs
@@ -29,202 +29,80 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtUserId = new System.Windows.Forms.TextBox();
-            this.txtUserPwd = new System.Windows.Forms.MaskedTextBox();
-            this.txtUserEmail = new System.Windows.Forms.MaskedTextBox();
-            this.txtUserTel = new System.Windows.Forms.MaskedTextBox();
-            this.txtUserBirth = new System.Windows.Forms.MaskedTextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtUserGender = new System.Windows.Forms.MaskedTextBox();
-            this.btnRegister = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.chkTrainer = new System.Windows.Forms.CheckBox();
-            this.chkMember = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.chkMember);
-            this.groupBox1.Controls.Add(this.chkTrainer);
-            this.groupBox1.Controls.Add(this.txtUserName);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.btnRegister);
-            this.groupBox1.Controls.Add(this.txtUserGender);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.txtUserBirth);
-            this.groupBox1.Controls.Add(this.txtUserTel);
-            this.groupBox1.Controls.Add(this.txtUserEmail);
-            this.groupBox1.Controls.Add(this.txtUserPwd);
-            this.groupBox1.Controls.Add(this.txtUserId);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(26, 30);
+            this.groupBox1.Location = new System.Drawing.Point(171, 99);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(379, 414);
+            this.groupBox1.Size = new System.Drawing.Size(379, 533);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "회원가입";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label5
+            // label1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(85, 265);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 12);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "이메일";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(73, 235);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 12);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "전화번호";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(73, 176);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 12);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "생년월일";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 148);
+            this.label2.Location = new System.Drawing.Point(70, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(38, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "비밀번호";
+            this.label2.Text = "label2";
             // 
-            // label1
+            // label3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(85, 121);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "아이디";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
             // 
-            // txtUserId
+            // label4
             // 
-            this.txtUserId.Location = new System.Drawing.Point(139, 118);
-            this.txtUserId.Name = "txtUserId";
-            this.txtUserId.Size = new System.Drawing.Size(131, 21);
-            this.txtUserId.TabIndex = 5;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(186, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
             // 
-            // txtUserPwd
+            // label5
             // 
-            this.txtUserPwd.Location = new System.Drawing.Point(139, 145);
-            this.txtUserPwd.Name = "txtUserPwd";
-            this.txtUserPwd.Size = new System.Drawing.Size(131, 21);
-            this.txtUserPwd.TabIndex = 6;
-            // 
-            // txtUserEmail
-            // 
-            this.txtUserEmail.Location = new System.Drawing.Point(139, 262);
-            this.txtUserEmail.Name = "txtUserEmail";
-            this.txtUserEmail.Size = new System.Drawing.Size(131, 21);
-            this.txtUserEmail.TabIndex = 7;
-            // 
-            // txtUserTel
-            // 
-            this.txtUserTel.Location = new System.Drawing.Point(139, 232);
-            this.txtUserTel.Name = "txtUserTel";
-            this.txtUserTel.Size = new System.Drawing.Size(131, 21);
-            this.txtUserTel.TabIndex = 8;
-            // 
-            // txtUserBirth
-            // 
-            this.txtUserBirth.Location = new System.Drawing.Point(139, 173);
-            this.txtUserBirth.Name = "txtUserBirth";
-            this.txtUserBirth.Size = new System.Drawing.Size(131, 21);
-            this.txtUserBirth.TabIndex = 9;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(85, 206);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(29, 12);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "성별";
-            // 
-            // txtUserGender
-            // 
-            this.txtUserGender.Location = new System.Drawing.Point(139, 203);
-            this.txtUserGender.Name = "txtUserGender";
-            this.txtUserGender.Size = new System.Drawing.Size(131, 21);
-            this.txtUserGender.TabIndex = 11;
-            // 
-            // btnRegister
-            // 
-            this.btnRegister.Location = new System.Drawing.Point(195, 305);
-            this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(75, 23);
-            this.btnRegister.TabIndex = 12;
-            this.btnRegister.Text = "회원가입";
-            this.btnRegister.UseVisualStyleBackColor = true;
-            this.btnRegister.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(97, 90);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 12);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "이름";
-            // 
-            // txtUserName
-            // 
-            this.txtUserName.Location = new System.Drawing.Point(139, 87);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(131, 21);
-            this.txtUserName.TabIndex = 14;
-            // 
-            // chkTrainer
-            // 
-            this.chkTrainer.AutoSize = true;
-            this.chkTrainer.Location = new System.Drawing.Point(87, 366);
-            this.chkTrainer.Name = "chkTrainer";
-            this.chkTrainer.Size = new System.Drawing.Size(72, 16);
-            this.chkTrainer.TabIndex = 15;
-            this.chkTrainer.Text = "직원전용";
-            this.chkTrainer.UseVisualStyleBackColor = true;
-            // 
-            // chkMember
-            // 
-            this.chkMember.AutoSize = true;
-            this.chkMember.Location = new System.Drawing.Point(195, 366);
-            this.chkMember.Name = "chkMember";
-            this.chkMember.Size = new System.Drawing.Size(72, 16);
-            this.chkMember.TabIndex = 16;
-            this.chkMember.Text = "회원전용";
-            this.chkMember.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
             // 
             // register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 488);
+            this.ClientSize = new System.Drawing.Size(800, 783);
             this.Controls.Add(this.groupBox1);
             this.Name = "register";
             this.Text = "register";
@@ -242,17 +120,5 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnRegister;
-        private System.Windows.Forms.MaskedTextBox txtUserGender;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.MaskedTextBox txtUserBirth;
-        private System.Windows.Forms.MaskedTextBox txtUserTel;
-        private System.Windows.Forms.MaskedTextBox txtUserEmail;
-        private System.Windows.Forms.MaskedTextBox txtUserPwd;
-        private System.Windows.Forms.TextBox txtUserId;
-        private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.CheckBox chkMember;
-        private System.Windows.Forms.CheckBox chkTrainer;
     }
 }
--- 비주얼프로젝트_20222940박경민/비주얼프로젝트_20222940박경민/UI/LOGIN/register.cs
+++ 비주얼프로젝트_20222940박경민/비주얼프로젝트_20222940박경민/UI/LOGIN/register.cs
@@ -26,20 +26,5 @@ namespace 비주얼프로젝트_20222940박경민
         {
 
         }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
